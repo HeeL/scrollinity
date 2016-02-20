2d5cf2c181ba9c5f6f5cd020ad4727eb5a2a053f
Fred Ngo <fredngo@gmail.com>
2016-02-19 22:53:09 -0800

    Fix var declaration for strict mode.

--- vendor/assets/javascripts/scrollinity.js
+++ vendor/assets/javascripts/scrollinity.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
   function load_new_items(){
-    sign = load_path.indexOf('?') >= 0 ? '&' : '?'
+    var sign = load_path.indexOf('?') >= 0 ? '&' : '?'
     $.get(load_path + sign + 'page=' + (++page_num), function(data, e) {
       if(data.length < 5) {
-        page_num = 0;
+        var page_num = 0;
         return false;
       }
       data_container.append(data);
@@ -22,11 +22,11 @@ $(document).ready(function(){
   }
 
   if($('*[data-scrollinity-path]').length > 0) {
-    var page_num = 1
-    load_path = $('*[data-scrollinity-path]').data('scrollinity-path');
-    loading_pic = $('*[data-scrollinity-loading-pic]');
-    data_container = $('#' + $('*[data-scrollinity-data-container]').data('scrollinity-data-container'));
-    bottom_px_limit = $('*[data-scrollinity-bottom-px-limit]').data('scrollinity-bottom-px-limit');
+    var page_num        = 1
+    var load_path       = $('*[data-scrollinity-path]').data('scrollinity-path');
+    var loading_pic     = $('*[data-scrollinity-loading-pic]');
+    var data_container  = $('#' + $('*[data-scrollinity-data-container]').data('scrollinity-data-container'));
+    var bottom_px_limit = $('*[data-scrollinity-bottom-px-limit]').data('scrollinity-bottom-px-limit');
 
     $(window).on('scroll', function(){
       if(loading_hidden() && near_bottom()) {
